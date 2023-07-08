5216f7dbf312114041066ed94ef3d6f9c6b29378
abnerfirmino <abnercasteliano@outlook.com>
2023-07-07 23:52:02 -0300

    configurando o vitest e vitest coverage

--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,10 @@ export default defineConfig({
     setupFiles: ['/.test/setup.js'],
     include: ['**/(*.)?{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     exclude: ['node_modules', 'dist', '.idea', '.git', '.cache'],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      include: ['src/**/*'],
+      exclude: [],
+    },
   },
 });

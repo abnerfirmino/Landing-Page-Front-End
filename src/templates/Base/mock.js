a6f2df20618878dbfbcedcd988483b2b4a7cb65e
abnerfirmino <abnercasteliano@outlook.com>
2023-07-10 19:48:08 -0300

    Template Base criado

---
+++ src/templates/Base/mock.js
@@ -0,0 +1,10 @@
+import linksMock from '../../components/NavBar/mock';
+
+export default {
+  links: linksMock,
+  logoData: {
+    text: 'Logo',
+    link: '#',
+  },
+  footerHtml: '<p>Teste de footer</p>',
+};

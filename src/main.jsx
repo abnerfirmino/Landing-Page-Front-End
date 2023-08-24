2661b737bff7543dab8916781deeb192e2ec034b
abnerfirmino <abnercasteliano@outlook.com>
2023-08-24 20:08:19 -0300

    main jsx ajustado

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+
 import { GlobalStyles } from './styles/global-styles';
 import { theme } from './styles/theme';
 import Home from './templates/Home';
@@ -8,8 +10,12 @@ import Home from './templates/Home';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
       <GlobalStyles />
-      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 );

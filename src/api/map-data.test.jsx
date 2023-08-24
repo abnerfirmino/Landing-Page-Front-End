081d91d6a8ff343d831574c6577a140375692427
abnerfirmino <abnercasteliano@outlook.com>
2023-08-24 09:56:10 -0300

    mapeando o menu

--- src/api/map-data.test.jsx
+++ src/api/map-data.test.jsx
@@ -8,8 +8,6 @@ describe('map-data', () => {
     expect(pagesData.title).toBe('');
     expect(pagesData.slug).toBe('');
     expect(pagesData.footerHtml).toBe('');
-    expect(pagesData.sections).toEqual([]);
-    expect(pagesData.menu).toEqual({});
   });
 
   it('should map data if there are data', () => {
@@ -18,14 +16,10 @@ describe('map-data', () => {
         title: 'title',
         slug: 'slug',
         footer_text: '<p>Hey</p>',
-        sections: [1, 2, 3, 4],
-        menu: { a: 'b' },
       },
     ])[0];
     expect(pagesData.title).toBe('title');
     expect(pagesData.slug).toBe('slug');
     expect(pagesData.footerHtml).toBe('<p>Hey</p>');
-    expect(pagesData.sections).toEqual([1, 2, 3, 4]);
-    expect(pagesData.menu).toEqual({ a: 'b' });
   });
 });

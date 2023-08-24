e44e2659a6ada8a5f6315460c2bbd6effd8fe818
abnerfirmino <abnercasteliano@outlook.com>
2023-08-24 19:59:54 -0300

    dados refatorados

--- src/components/Footer/Footer.stories.jsx
+++ src/components/Footer/Footer.stories.jsx
@@ -4,7 +4,7 @@ export default {
   title: 'Footer',
   component: Footer,
   args: {
-    footerHtml: `<p><a href="https://beacons.page/otaviomiranda">Feito com ❤ por Abner Firmino</a></p>`,
+    footerHtml: `<p><a href="https://beacons.page/abnerfirmino">Feito com ❤ por Abner Firmino</a></p>`,
   },
 };
 

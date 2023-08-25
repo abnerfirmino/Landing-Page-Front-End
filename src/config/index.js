bbd285b283cfc6f86df3d4ac573203f9fff29ee5
abnerfirmino <abnercasteliano@outlook.com>
2023-08-25 01:48:39 -0300

    config refatorado

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export default {
-  url: 'https://strapi-landing-pages-y39x.onrender.com/api/pages?populate=deep',
-  siteName: 'Abner Firmino',
+  url: 'https://strapi-landing-pages-y39x.onrender.com/api/pages/?slug=',
+  siteName: 'Landing Page',
   defaultSlug: 'landing-page',
 };

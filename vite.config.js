da826b25294cf6b38535885959e3e23a4c910ec6
abnerfirmino <abnercasteliano@outlook.com>
2023-08-26 01:09:46 -0300

    config

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   root: '.',
-  build: {
+/*   build: {
     outDir: '/dist',
-  },
+  }, */
   test: {
     globals: true,
     environment: 'jsdom',
